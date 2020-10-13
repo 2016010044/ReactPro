1ceef85745b8ffbc71e6259d12c62ade121d1837
0115118 <lihu@bonc.com.cn>
2020-10-13 19:46:21 +0800

    react项目提交

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+const config = {
+     // 数据库信息配置
+    database: {
+        HOST:  'localhost', // 主机
+        PORT:  '3306', // 端口
+        USER:  'root', // MySQL认证的用户名
+        PASSWORD:  'lh110911', // MySQL认证的密码
+        DATABASE:  'itlikeedu' // 数据库
+    },
+     // 其它配置
+    KEY: 'iTlike@#*09221``~~---++===yyyjhh'
+     // ....
+};
+
+module.exports = config;
